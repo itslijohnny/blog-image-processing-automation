7cd53d14f41583eca20a2517232b574e949060da
Iamjohnny <5987932+iamjohnnyli@users.noreply.github.com>
2019-05-11 22:15:58 -0700

    v 0.1

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# blog-image-processing
+This script is for automating the process of cropping, resizing, adding author's information, compressing, and uploading an image.
+
+- The compressing service I use [tinypng](https://tinypng.com)
+- The storage for image I user [sm.ms](https://sm.ms/)
+- This script works only for the image download from [unsplash.com](https://unsplash.com/), since I use regular expression operations to get the author's name from file name.
+
+## Usage
+
+To use correctly, you need to create an ```api.txt``` file that contains only the API key for  [tinypng](https://tinypng.com). To generate an API key, you can check [https://tinypng.com/developers](https://tinypng.com/developers).
+
+```main.py``` can be easily run in the terminal with the command:
+```bash
+python main.py
+```
+Once it launched, you can drag and drop any image on it.
+
+
+```automator.py``` can be used in Mac's Automator. By building a server with Automator, I can right click on any image and run the script, or use a shortcut to run the script.
---
+++ main.py
@@ -0,0 +1,67 @@
+from PIL import Image, ImageDraw, ImageFont
+import tinify
+import requests
+import sys
+import os
+import os.path
+import re
+import pyperclip
+import json
+
+
+size = 1600, 728
+x_text = 38
+y_text = 682
+font = ImageFont.truetype('/Library/Fonts/Arial.ttf', 24)
+
+
+def img_process(img,size,x_text,y_text,font):
+    name=str(os.path.basename(img)).rstrip()
+    img = img.rstrip()
+    original = Image.open(img)
+    cropped = img_crop(original,size)
+    cropped.thumbnail(size, Image.ANTIALIAS)
+    img_addtext(cropped,name,x_text,y_text,font)
+    cropped.save('resizeimage.jpg', "JPEG")
+    keyFile = open('api.txt', 'r')
+    consumer_key = keyFile.readline().rstrip()
+    tinify.key = consumer_key
+    source = tinify.from_file("resizeimage.jpg")
+    source.to_file("optimized.jpg")
+    print('Compressed by tinypng')
+    img2 = 'optimized.jpg'
+    url = "https://sm.ms/api/upload"
+    files = {'smfile': ("%s"%name , open(img2.replace("\n", ""), 'rb'), 'image/png')}
+    sdata = {'ssl': 1}
+    res = requests.post(url=url, data=sdata, files=files)
+    the_json = res.text
+    the_json = json.loads(the_json)
+    print('Uploaded to sm.ms')
+    print(the_json["data"]["url"])  # 图片链接
+    mk = '![%s](%s )' % (the_json["data"]["filename"], the_json["data"]["url"])
+    pyperclip.copy(mk)
+    print("copyed in clipboard")
+
+
+def img_crop(original,size):
+    width, height = original.size
+    left = 0
+    right = width
+    h = int((width/size[0])*size[1])
+    top = (height - h)/2
+    bottom = h+(height - h)/2
+    cropped = original.crop((left, top, right, bottom))
+    return cropped
+
+def img_addtext(cropped,name,x_text,y_text,font):
+    (x, y) = (x_text, y_text)
+    ste = (re.findall(r'.+(?=[a-z])', re.findall(r'.+(?<=[0-9])', name)[0])[0]).replace('-',' ').title()
+    message = "Photo by {} on Unsplash".format(ste)
+    color = 'rgb(255, 255, 255)' # black color
+    d = ImageDraw.Draw(cropped)
+    d.text((x, y), message, fill=color, font=font)
+
+
+while(True):
+    img = sys.stdin.readline()
+    img_process(img,size,x_text,y_text,font)
